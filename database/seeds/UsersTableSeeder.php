e8c8daa97806db0da9dd66ead7edf166792a6eea
eleven9013 <guanhua0821@outlook.com>
2018-02-03 04:10:52 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->email = 'gugh@uc55.com';
         $user->password = bcrypt('123456');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
